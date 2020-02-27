87d66566a9c4533eee6fb52be0113c2de49da27e
unknown <gummi.bear456aaaaa@gmail.com>
2020-02-28 02:26:06 +0900

    0228

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private RectTransform playerRect;
+    private readonly float moveDistance = 5f;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        playerRect = this.GetComponent<RectTransform>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            playerRect.anchoredPosition = new Vector2(playerRect.localPosition.x, playerRect.localPosition.y + moveDistance);
+        }
+    }
+}
